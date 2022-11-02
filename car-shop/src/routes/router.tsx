b7a955436192550d23a1c93a7fdac15c2cda8124
Robson Fernando <robson.2501@hotmail.com>
2022-11-02 15:32:39 -0300

    feat: pages folder added

--- car-shop/src/routes/router.tsx
+++ car-shop/src/routes/router.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import Home from "../pages/home/home"
+import Home from "../pages/home/index"
 
 const Router = () => {
   return (
@@ -7,8 +7,6 @@ const Router = () => {
       <Routes>
     
         <Route path='/' element={<Home/>}/>
-        <Route path='/countries' element={<CountriesOrdered/>}/>
-        <Route path='/characters/:id' element={<Char/>}/>
 
       </Routes>
     </BrowserRouter>
